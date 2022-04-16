5936f39bfed677caffdb03d633eb1d9f52ef172f
assraf-nur <assraf.nur@gmail.com>
2022-04-17 01:43:52 +0600

    added a banner AF

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -4,6 +4,7 @@ import Banner from './Banner/Banner';
 const Home = () => {
     return (
         <div className='container'>
+            <h1>ache</h1>
             <Banner></Banner>
         </div>
     );
