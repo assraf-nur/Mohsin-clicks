2870b63a0d655a037acdff8c5094af58d69e4f31
assraf-nur <assraf.nur@gmail.com>
2022-04-17 01:05:01 +0600

    added a navbar in header section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Navbar from './Components/Header/Navbar/Navbar';
+import Blogs from './Components/Home/Blogs/Blogs';
+import CustomerReview from './Components/Home/CustomerReview/CustomerReview';
+import Home from './Components/Home/Home'
+import Service from './Components/Home/Service/Service'
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Navbar></Navbar>
+      <Routes>
+        <Route path='/home' element={<Home></Home>}/>
+        <Route path='/service' element={<Service></Service>}/>
+        <Route path='/review' element={<CustomerReview></CustomerReview>}/>
+        <Route path='/blogs' element={<Blogs></Blogs>}/>
+      </Routes>
     </div>
   );
 }
--- src/Components/Header/Navbar/Navbar.js
+++ src/Components/Header/Navbar/Navbar.js
@@ -1,66 +1,60 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-export default function Menu({ fixed }) {
-  const [menuOpen, setMenuOpen] = React.useState(false);
+const Navbar = () => {
   return (
-    <>
-      <div className="flex flex-wrap py-2">
-        <div className="w-full px-4">
-          <nav className="relative flex flex-wrap items-center justify-between px-2 py-3 bg-slate-500 rounded">
-            <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
-              <div className="w-full relative flex justify-between lg:w-auto px-4 lg:static lg:block lg:justify-start">
-                <a
-                  className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white"
-                  href="#pablo"
-                >
-                  slate Starter Menu
-                </a>
-                <button
-                  className="text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
-                  type="button"
-                  onClick={() => setMenuOpen(!menuOpen)}
-                >
-                  <i className="fas fa-bars"></i>
-                </button>
-              </div>
-              <div
-                className={
-                  "lg:flex flex-grow items-center" +
-                  (menuOpen ? " flex" : " hidden")
-                }
-                id="example-navbar-info"
-              >
-                <ul className="flex flex-col lg:flex-row list-none lg:ml-auto">
-                  <li className="nav-item">
-                    <a
-                      className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
-                      href="#pablo"
-                    >
-                      Discover
-                    </a>
-                  </li>
-                  <li className="nav-item">
-                    <a
-                      className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
-                      href="#pablo"
-                    >
-                      Profile
-                    </a>
-                  </li>
-                  <li className="nav-item">
-                    <a
-                      className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
-                      href="#pablo"
-                    >
-                      Settings
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </nav>
+    <nav className=" bg-slate-200 container mx-auto border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800">
+      <div className="container flex flex-wrap justify-between items-center mx-auto">
+         <Link className=" text-xl font-bold" to='/home'>Butterfly-Clicks</Link>
+        <button
+          data-collapse-toggle="mobile-menu"
+          type="button"
+          className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+          aria-controls="mobile-menu"
+          aria-expanded="false"
+        >
+          <span className="sr-only">Open main menu</span>
+          <svg
+            className="w-6 h-6"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fillRule="evenodd"
+              d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+              clipRule="evenodd"
+            ></path>
+          </svg>
+          <svg
+            className="hidden w-6 h-6"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fillRule="evenodd"
+              d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+              clipRule="evenodd"
+            ></path>
+          </svg>
+        </button>
+        <div className="hidden w-full md:block md:w-auto" id="mobile-menu">
+          <ul className="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
+            <li>
+              <Link to='/home'>Home</Link>
+            </li>
+            <li>
+              <Link to='/review'>Review</Link>
+            </li>
+            <li>
+              <Link to='/blogs'>Blogs</Link>
+            </li>
+          </ul>
         </div>
       </div>
-    </>
+    </nav>
   );
-}
+};
+
+export default Navbar;
