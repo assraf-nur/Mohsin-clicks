75a816c9826a4f4467ce62abe8ec97cb899b25b2
assraf-nur <assraf.nur@gmail.com>
2022-04-17 17:40:46 +0600

    add a require auth

--- src/Components/LoginRegistration/Login/Login.js
+++ src/Components/LoginRegistration/Login/Login.js
@@ -1,9 +1,18 @@
 import React, { useRef } from "react";
 import { Button, Form } from "react-bootstrap";
+import { useSignInWithEmailAndPassword } from "react-firebase-hooks/auth";
 import { useNavigate } from "react-router-dom";
+import auth from "../../../firebase.init";
 import "./Login.css";
 
 const Login = () => {
+  const [
+    signInWithEmailAndPassword,
+    user,
+    loading,
+    error,
+  ] = useSignInWithEmailAndPassword(auth);
+
   const emailRef = useRef("");
   const passwordRef = useRef("");
   const navigate = useNavigate();
@@ -12,7 +21,12 @@ const Login = () => {
     e.preventDefault();
     const email = emailRef.current.value;
     const password = passwordRef.current.value;
+    signInWithEmailAndPassword(email, password);
   };
+
+  if(user){
+    navigate('/home')
+  }
 
   const navigateRegister = (e) => {
     navigate("/register");
--- src/Components/LoginRegistration/Registration/Registration.js
+++ src/Components/LoginRegistration/Registration/Registration.js
@@ -2,17 +2,33 @@ import React from "react";
 import { Button, Form } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 import './Registration.css'
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import auth from "../../../firebase.init";
 
 const Registration = () => {
+    const [
+      createUserWithEmailAndPassword,
+      user,
+      loading,
+      error,
+    ] = useCreateUserWithEmailAndPassword(auth);
 
     const navigate = useNavigate();
 
     const navigateLogin = e =>{
         navigate('/login');
     }
+    if(user){
+      navigate('/home');
+    }
 
     const handleRegister = e =>{
         e.preventDefault();
+        const name = e.target.name.value;
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+
+        createUserWithEmailAndPassword(email, password);
     }
 
   return (
@@ -21,12 +37,12 @@ const Registration = () => {
       <Form onSubmit={handleRegister}>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Your Name</Form.Label>
-          <Form.Control type="text" placeholder="Enter Name" required/>
+          <Form.Control name="name" type="text" placeholder="Enter Name" required/>
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" required/>
+          <Form.Control name="email" type="email" placeholder="Enter email" required/>
           <Form.Text className="text-muted">
             We'll never share your email with anyone else.
           </Form.Text>
@@ -34,7 +50,7 @@ const Registration = () => {
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" required/>
+          <Form.Control name="password" type="password" placeholder="Password" required/>
           <Form.Text className="text-muted">
             We'll never share your password with anyone else.
           </Form.Text>
